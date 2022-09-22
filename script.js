997e7556e16d1f46d2809acb49c33dc7765469ca
Shubham12399 <112411664+Shubham12399@users.noreply.github.com>
2022-09-22 13:49:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let tagItem = document.querySelectorAll(".tag-item");
+let items = document.querySelectorAll(".items");
+let i;
+function openUlmenu(){
+    let ul = document.querySelector("#menu");
+    ul.classList.toggle("open-ul");
+}
