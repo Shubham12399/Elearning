dd7de6846f1cc40e95f2ec624116ce1015599079
Shubham12399 <112411664+Shubham12399@users.noreply.github.com>
2022-09-24 11:56:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let tagItem = document.querySelectorAll(".tag-item");
-let items = document.querySelectorAll(".items");
-let i;
-function openUlmenu(){
-    let ul = document.querySelector("#menu");
-    ul.classList.toggle("open-ul");
-}
